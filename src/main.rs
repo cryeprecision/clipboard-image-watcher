39939bbe2636a6f38f6dc400af506c584e5107b3
cryeprecision <muench.rico@gmail.com>
2024-01-13 08:48:40 +0100

    refactor and add comments

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
 mod util;
 
 use anyhow::Context;
-use util::*;
-//
 use windows::core::*;
 use windows::Win32::Foundation::*;
 use windows::Win32::System::DataExchange::*;
 use windows::Win32::System::LibraryLoader::*;
 use windows::Win32::UI::WindowsAndMessaging::*;
 
+use crate::util::*;
+
 const CLASS_NAME: PCSTR = s!("ClipboardWatcher");
 
+/// Invoked every time the clipboard content changes
 unsafe fn on_clipboard_update() {
     let Ok(bitmap) = clipboard_bitmap() else {
         return;
     };
-    if let Err(err) = clipboard_save_image(&bitmap) {
+    if let Err(err) = save_image_png(&bitmap) {
         eprintln!("couldn't save clipboard image: {:?}", err);
     }
 }
 
-unsafe extern "system" fn window_proc(
+/// <https://learn.microsoft.com/en-us/windows/win32/learnwin32/writing-the-window-procedure>
+#[allow(non_snake_case)]
+unsafe extern "system" fn WindowProc(
     hwnd: HWND,
     msg: u32,
     w_param: WPARAM,
@@ -28,27 +31,35 @@ unsafe extern "system" fn window_proc(
 ) -> LRESULT {
     match msg {
         // https://learn.microsoft.com/en-us/windows/win32/winmsg/wm-create
-        WM_CREATE => AddClipboardFormatListener(hwnd)
-            .context("couldn't add clipboard format listener")
-            .unwrap(),
+        WM_CREATE => {
+            AddClipboardFormatListener(hwnd)
+                .context("couldn't add clipboard format listener")
+                .unwrap();
+        }
         // https://learn.microsoft.com/en-us/windows/win32/winmsg/wm-destroy
-        WM_DESTROY => RemoveClipboardFormatListener(hwnd)
-            .context("couldn't remove clipboard format listener")
-            .unwrap(),
+        WM_DESTROY => {
+            RemoveClipboardFormatListener(hwnd)
+                .context("couldn't remove clipboard format listener")
+                .unwrap();
+        }
         // https://learn.microsoft.com/en-us/windows/win32/dataxchg/wm-clipboardupdate
-        WM_CLIPBOARDUPDATE => on_clipboard_update(),
+        WM_CLIPBOARDUPDATE => {
+            on_clipboard_update();
+        }
         // ignore all other messages
         _ => (),
     }
+
+    // https://learn.microsoft.com/en-us/windows/win32/learnwin32/writing-the-window-procedure#default-message-handling
     DefWindowProcA(hwnd, msg, w_param, l_param)
 }
 
 fn main() {
-    // https://stackoverflow.com/a/65857206
+    // based on https://stackoverflow.com/a/65857206
     unsafe {
         let mut window = std::mem::zeroed::<WNDCLASSEXA>();
         window.cbSize = std::mem::size_of::<WNDCLASSEXA>() as u32;
-        window.lpfnWndProc = Some(window_proc);
+        window.lpfnWndProc = Some(WindowProc);
         window.lpszClassName = CLASS_NAME;
 
         if RegisterClassExA(&window as *const WNDCLASSEXA) == 0 {
@@ -60,19 +71,20 @@ fn main() {
             .context("couldn't get handle to own module")
             .unwrap();
 
+        // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexa
         let handle = CreateWindowExA(
-            WINDOW_EX_STYLE(0),
-            CLASS_NAME,
-            PCSTR(std::ptr::null()),
-            WINDOW_STYLE(0),
-            0,
-            0,
-            0,
-            0,
-            HWND_MESSAGE,
-            HMENU(0),
-            module,
-            None,
+            WINDOW_EX_STYLE(0),      // Optional window styles.
+            CLASS_NAME,              // Window class
+            PCSTR(std::ptr::null()), // Window text
+            WINDOW_STYLE(0),         // Window style
+            CW_USEDEFAULT,           // Size and position
+            CW_USEDEFAULT,           // Size and position
+            CW_USEDEFAULT,           // Size and position
+            CW_USEDEFAULT,           // Size and position
+            HWND_MESSAGE,            // Create a message-only window
+            HMENU(0),                // Menu
+            module,                  // Instance handle
+            None,                    // Additional application data
         );
 
         if handle == HWND(0) {
--- src/util.rs
+++ src/util.rs
@@ -3,19 +3,22 @@ use std::ffi::c_void;
 use anyhow::Context;
 use image::codecs::png::PngEncoder;
 use image::{DynamicImage, ImageFormat};
-//
 use windows::Win32::Foundation::*;
 use windows::Win32::Graphics::Gdi::*;
 use windows::Win32::System::DataExchange::*;
 use windows::Win32::System::Memory::*;
 use windows::Win32::System::Ole::*;
 
+/// Try to read a bitmap image from the clipboard.
 pub unsafe fn clipboard_bitmap() -> anyhow::Result<DynamicImage> {
+    // check for image content in the clipboard
     IsClipboardFormatAvailable(CF_DIB.0.into())
         .context("clipboard format CF_DIB is not available")?;
 
+    // get access to the clipboard and prevent other applications fro modifying it
     OpenClipboard(HWND(0)).context("couldn't open clipboard")?;
 
+    // get a handle to the clipboard data as a device-independent bitmap (DIB)
     let handle = match GetClipboardData(CF_DIB.0.into()) {
         Ok(handle) => handle.0 as *mut c_void,
         Err(err) => {
@@ -24,12 +27,14 @@ pub unsafe fn clipboard_bitmap() -> anyhow::Result<DynamicImage> {
         }
     };
 
+    // lock the DIB memory object
     let data_ptr = GlobalLock(HGLOBAL(handle));
     if data_ptr.is_null() {
         let _ = CloseClipboard();
         return Err(anyhow::anyhow!("couldn't lock clipboard data"));
     }
 
+    // get the size of the DIB memory object
     let data_len = GlobalSize(HGLOBAL(handle));
     if data_len < std::mem::size_of::<BITMAPINFO>() {
         let _ = GlobalUnlock(HGLOBAL(data_ptr));
@@ -37,6 +42,7 @@ pub unsafe fn clipboard_bitmap() -> anyhow::Result<DynamicImage> {
         return Err(anyhow::anyhow!("clipboard data is malformed"));
     }
 
+    // view the DIP memory object as a block of bytes
     let data = std::slice::from_raw_parts(data_ptr as *const u8, data_len);
 
     // read the bitmap from the clipboard, prepending the file header
@@ -54,34 +60,38 @@ pub unsafe fn clipboard_bitmap() -> anyhow::Result<DynamicImage> {
             bfReserved1: 0,
             // Reserved; must be zero.
             bfReserved2: 0,
-            // The offset, in bytes, from the beginning of the BITMAPFILEHEADER structure
-            // to the bitmap bits.
+            // The byte offset from the BITMAPFILEHEADER structure to the bitmap bits.
             bfOffBits: std::mem::size_of::<BITMAPFILEHEADER>() as u32
                 + std::mem::size_of::<BITMAPINFOHEADER>() as u32,
         };
+
+        // view the BITMAPFILEHEADER object as a block of bytes
         let header_bytes = std::slice::from_raw_parts(
             &header as *const BITMAPFILEHEADER as *const u8,
             std::mem::size_of::<BITMAPFILEHEADER>(),
         );
 
+        // copy the header and the bitmap into the buffer and return the buffer
         buffer[..std::mem::size_of::<BITMAPFILEHEADER>()].copy_from_slice(header_bytes);
         buffer[std::mem::size_of::<BITMAPFILEHEADER>()..].copy_from_slice(data);
-
         buffer
     };
 
+    // unlock the DIB memory object
     if let Err(err) = GlobalUnlock(HGLOBAL(data_ptr)) {
         let _ = CloseClipboard();
         return Err(err).context("couldn't unlock clipboard data");
     }
 
+    // remove access to the clipboard and allow other applications to modify the clipboard
     CloseClipboard().context("couldn't close clipboard")?;
 
     image::load_from_memory_with_format(&bitmap, ImageFormat::Bmp)
         .context("couldn't parse clipboard image")
 }
 
-pub unsafe fn clipboard_save_image(image: &DynamicImage) -> anyhow::Result<()> {
+/// Write encode an image in the PNG format and write it to disk.
+pub unsafe fn save_image_png(image: &DynamicImage) -> anyhow::Result<()> {
     let filename = format!(
         "clip_{}.png",
         std::time::SystemTime::now()
